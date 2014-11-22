ddb3b1dd60e6925b1d31c762d3fe96fb4214b2e2
Simon Lydell <simon.lydell@gmail.com>
2014-11-22 15:48:47 +0100

    Cleanup main.js

--- app/main.js
+++ app/main.js
@@ -1,35 +1,15 @@
-
-
+var Phone = require("phone");
+var randomAngle = require("randomAngle");
 
 var settings = {
   tickrate: 40, // bpm
   tolerance: 15
 };
 
-
-var audioContext;
-window.addEventListener('load', initAudio, false);
-function initAudio() {
-  try {
-    // Fix up for prefixing
-    window.AudioContext = window.AudioContext||window.webkitAudioContext;
-    audioContext = new AudioContext();
-  }
-  catch(e) {
-    console.log('Web Audio API is not supported in this browser');
-  }
-}
-
-var Phone = require("phone");
 var phone = new Phone({ tolerance: settings.tolerance });
-
-var randomAngle = require("randomAngle");
-
-var lastAlpha = 0;
+document.body.appendChild(phone.element);
 
 var lightGreen = "rgba(60, 192, 40, 0.4)";
-var green = "rgba(0, 128, 0, 1)";
-var red = "rgba(200, 0, 0, 0.5)";
 var white = "white";
 
 function msInterval(tickrate) {
@@ -40,28 +20,18 @@ function bodyBackground(c) {
   document.body.style.backgroundColor = c;
 }
 
-var refreshInterval = msInterval(settings.tickrate);
+var lastAlpha = 0;
 
 FULLTILT.getDeviceOrientation({type: "game"})
   .then(function(deviceOrientation) {
     deviceOrientation.listen(function() {
-      // do things with deviceOrientation
       var euler = deviceOrientation.getScreenAdjustedEuler();
-
       lastAlpha = euler.alpha;
-
-  console.log(lastAlpha, phone.getAngle());
     });
   })
   .catch(function(error) {
     console.log(error);
   });
-
-document.body.appendChild(phone.element);
-
-// window.addEventListener("click", function() {
-//   phone.setAngle((phone.getAngle() + 45) % 360);
-// }, true)
 
 function tick() {
   phone.incrementTicks();
@@ -73,16 +43,11 @@ function tick() {
   }
   phone.setAngle(randomAngle());
 }
-
-// Beat:
-var timer = setInterval(tick, refreshInterval);
+setInterval(tick, msInterval(settings.tickrate));
 
 function draw() {
   phone.redraw(lastAlpha);
-
   bodyBackground(phone.isEqualRotation(lastAlpha) ? lightGreen : white);
-
   requestAnimationFrame(draw);
 }
-
 requestAnimationFrame(draw);
