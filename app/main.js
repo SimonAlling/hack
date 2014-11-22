f84ede5a1a99f6c5ae8c2bdef8fac8e9e0705116
Simon Lydell <simon.lydell@gmail.com>
2014-11-22 12:00:03 +0100

    Add a rotatable phone

--- app/main.js
+++ app/main.js
@@ -1,5 +1,14 @@
 var hello = require("hello");
+var Phone = require("phone");
 
 hello();
 
 console.log(FULLTILT);
+
+var phone = new Phone();
+
+document.body.appendChild(phone.element);
+
+window.addEventListener("click", function() {
+  phone.setRotation((phone.rotation + 45) % 360);
+}, true)
