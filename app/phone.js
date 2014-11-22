89f7d7f800a5dcdfe1412ee1977f914e514fcb56
Simon Lydell <simon.lydell@gmail.com>
2014-11-22 12:42:39 +0100

    Add initial isEqualRotation implementation

--- app/phone.js
+++ app/phone.js
@@ -1,13 +1,22 @@
-function Phone() {
+function Phone(options) {
+  options = options || {}
   this.element = document.createElement("div");
   this.element.classList.add("phone");
   this.rotation = 0;
+  this.threshold = options.threshold || 10
 }
 
 
 Phone.prototype.setRotation = function(deg) {
   this.rotation = deg;
   this.element.style.transform = "rotate(" + -deg + "deg)";
+};
+
+Phone.prototype.isEqualRotation = function(rotation) {
+  return (
+    rotation >= this.rotation - this.threshold &&
+    rotation <= this.rotation + this.threshold
+  )
 };
 
 module.exports = Phone;
